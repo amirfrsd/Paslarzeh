b8339f63f615a57ad3d2939f7da32c4255cef02e
Ahmad <sahmmadh@gmail.com>
2016-06-18 14:13:05 +0430

    Initial Commit

---
+++ Paslarzeh/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Paslarzeh
+//
+//  Created by Ahmad on 6/18/16.
+//  Copyright © 2016 Ahmad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
